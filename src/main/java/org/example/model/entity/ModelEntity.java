e3fa492604079a1c8fe0bf677683f8f6ba4ddf82
hommm5 <hommm5@gmail.com>
2024-01-16 11:33:34 +0200

    offer

--- src/main/java/org/example/model/entity/ModelEntity.java
+++ src/main/java/org/example/model/entity/ModelEntity.java
@@ -1,13 +1,40 @@
 package org.example.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.example.model.entity.enums.ModelCategoryEnum;
 
 @Entity
 @Table(name = "models")
 public class ModelEntity extends BaseEntity {
 
     private String name;
+    @Enumerated(EnumType.STRING)
+    private ModelCategoryEnum model;
 
-    private ModelCategory modelCategory;
+    @ManyToOne
+    private BrandEntity brand;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ModelCategoryEnum getModel() {
+        return model;
+    }
+
+    public void setModel(ModelCategoryEnum model) {
+        this.model = model;
+    }
+
+    public BrandEntity getBrand() {
+        return brand;
+    }
+
+    public void setBrand(BrandEntity brand) {
+        this.brand = brand;
+    }
 }
--- src/main/java/org/example/model/entity/OfferEntity.java
+++ src/main/java/org/example/model/entity/OfferEntity.java
@@ -1,9 +1,28 @@
 package org.example.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.example.model.entity.enums.EngineEnum;
+import org.example.model.entity.enums.Transmission;
+
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "offers")
 public class OfferEntity extends  BaseEntity{
+
+    private BigDecimal price;
+    private String imgUrl;
+
+    private long mileage;
+
+    private int year;
+
+    @Enumerated(EnumType.STRING)
+    private EngineEnum engine;
+    @Enumerated(EnumType.STRING)
+    private Transmission transmission;
+    private String description;
+    @ManyToOne
+    private ModelEntity model;
+
 }
