158bbae6ddb641c15fdb17a992e55723ecc18ddb
hommm5 <hommm5@gmail.com>
2024-01-17 07:18:29 +0200

    model, brand

--- src/main/java/org/example/model/entity/OfferEntity.java
+++ src/main/java/org/example/model/entity/OfferEntity.java
@@ -3,13 +3,18 @@ package org.example.model.entity;
 import jakarta.persistence.*;
 import org.example.model.entity.enums.EngineEnum;
 import org.example.model.entity.enums.Transmission;
+import org.hibernate.annotations.JdbcTypeCode;
 
 import java.math.BigDecimal;
+import java.sql.Types;
+import java.util.UUID;
 
 @Entity
 @Table(name = "offers")
 public class OfferEntity extends  BaseEntity{
 
+    @JdbcTypeCode(Types.VARCHAR)
+    private UUID uuid;
     private BigDecimal price;
     private String imgUrl;
 
@@ -25,4 +30,75 @@ public class OfferEntity extends  BaseEntity{
     @ManyToOne
     private ModelEntity model;
 
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public long getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(long mileage) {
+        this.mileage = mileage;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public EngineEnum getEngine() {
+        return engine;
+    }
+
+    public void setEngine(EngineEnum engine) {
+        this.engine = engine;
+    }
+
+    public Transmission getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(Transmission transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ModelEntity getModel() {
+        return model;
+    }
+
+    public void setModel(ModelEntity model) {
+        this.model = model;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
 }
--- src/main/java/org/example/web/OfferController.java
+++ src/main/java/org/example/web/OfferController.java
@@ -1,13 +1,24 @@
 package org.example.web;
 
+import org.example.model.dto.CreateOfferDto;
+import org.example.service.OfferService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/offers")
 public class OfferController {
+
+    private final OfferService offerService;
+
+    public OfferController(OfferService offerService) {
+        this.offerService = offerService;
+    }
 
     @GetMapping("/all")
     public String getAllOffers (){
@@ -19,8 +30,14 @@ public class OfferController {
         return "offer-add";
     }
 
-    @GetMapping("/{id}/details")
-    public String getDetails(@PathVariable("id")String uuid){
+    @PostMapping("add")
+    public String addOffer(CreateOfferDto createOfferDto){
+        offerService.createOffer(createOfferDto);
+
+        return "/";
+    }
+    @GetMapping("/{uuid}/details")
+    public String getDetails(@PathVariable("uuid") UUID uuid){
         return "details";
     }
 }
